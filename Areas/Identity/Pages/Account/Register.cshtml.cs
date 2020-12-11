f5be913f59fab18f3638261c71296386753342e5
M.Santos <miguel.santos.student@gmail.com>
2020-12-11 15:47:34 -0500

    Revert "added default role to all future users (employee)"
    This reverts commit 81fb9763c06b53255682a577239139fc98c16d28.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,7 +97,6 @@ namespace employee_management_system.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _userManager.AddToRoleAsync(user, "Employee").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
