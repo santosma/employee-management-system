81fb9763c06b53255682a577239139fc98c16d28
M.Santos <miguel.santos.student@gmail.com>
2020-12-11 05:48:28 -0500

    added default role to all future users (employee)

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,6 +97,7 @@ namespace employee_management_system.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, "Employee").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
